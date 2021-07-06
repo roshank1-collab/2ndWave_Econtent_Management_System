9f58ea9f896233dd0db90d721555effd2c483cef
Shankar BHandari <iamshankarbhandari@gmail.com>
2021-07-06 11:11:44 +0545

    create server

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors'); // to authorise the request from user or control the user request 
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended:false}));
+app.use(express.json())
+require('./database/db');
+
+app.use(cors()); // to get data from frontend 
+
+var server=app.listen(90, function (req, res) {
+    console.log('Listening to request on port 90');
+});
