6ab3398a6b992c03091b1d3e5e825c439a536f12
SujitPrasaddKushwaha <roniejo.rm@gmail.com>
2021-07-17 11:31:01 +0545

    slick slider for subscibe added

--- frontend/econtent/src/Dashboard/Dashboard.js
+++ frontend/econtent/src/Dashboard/Dashboard.js
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom'
 import axios from 'axios'
 import Subscribe from "./Channels/Subscribe";
 import DashboardUpperPart from "./DashboardUpperPart";
+import SimpleSlider from "./Channels/SlickSlider";
 
 class Dashboard extends Component {
     render() {
         return (
             <div>
                 <DashboardUpperPart />
+                {/* <SimpleSlider/> */}
                 <Subscribe />
             </div>
 
