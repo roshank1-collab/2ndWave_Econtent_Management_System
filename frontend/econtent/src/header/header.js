2d7e2cd6bb0cacc12e18601095de817374865e87
Roshan Koirala <koiralaroshan678@gmail.com>
2021-07-18 15:21:09 +0545

    header

--- frontend/econtent/src/header/header.js
+++ frontend/econtent/src/header/header.js
@@ -1,10 +1,10 @@
 import { Component } from "react"
-import { Nav, NavDropdown, Navbar, Form, Button, Container } from 'react-bootstrap';
-import { Modal, ModalDialog, ModalHeader, ModalTitle, ModalBody, ModalFooter, FormControl } from 'react-bootstrap'
-import { FaTwitter, FaUserGraduate, FaLinkedin, FaInstagram, FaCcVisa, FaAngellist, FaNewspaper, FaServicestack, FaTelegram, FaSignInAlt, FaBell, FaUsers, FaRocketchat, FaSignOutAlt, FaUserCircle, FaPlusCircle, FaRegEye, FaUpload, FaMedapps, FaTelegramPlane, FaUserCog, FaFacebook, FaRegSmile } from 'react-icons/fa'
-
+import { Nav, NavDropdown, Navbar, Form, Button } from 'react-bootstrap';
+import { FaUserGraduate, FaCcVisa, FaAngellist, FaNewspaper, FaServicestack, FaTelegram, FaSignInAlt, FaUsers, FaRegSmile } from 'react-icons/fa'
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+
+import { Modal, ModalDialog, ModalHeader, ModalTitle, ModalBody, ModalFooter} from 'react-bootstrap'
 
 const id = localStorage.getItem('id')
 
@@ -14,7 +14,7 @@ function Example() {
   const handleShow = () => setShow(true);
   const out = () => {
     localStorage.removeItem('token')
-    localStorage.removeItem('loginstatus')
+    localStorage.removeItem('loginstatus')    
     setShow(false)
   };
 
@@ -31,7 +31,7 @@ function Example() {
           <Button variant="secondary" onClick={handleClose}>
             No
           </Button>
-          <Button variant="primary" href="/login" onClick={out}>
+          <Button variant="primary" href= "/login" onClick={out}>
             Yes
           </Button>
         </Modal.Footer>
@@ -46,74 +46,49 @@ class Header extends Component {
   logout = () => {
     localStorage.removeItem('token')
     localStorage.removeItem('loginstatus')
-    window.location.href = '/'
+    window.locatin.href = '/'
   }
   render() {
     {
       if (localStorage.getItem('token') && localStorage.getItem('loginstatus') === 'true') {
         var menu =
 
-        <nav>
-<Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-  <Navbar.Toggle aria-controls="navbarScroll" />
-  <Navbar.Collapse id="navbarScroll">
-    <Nav
-      className="mr-auto my-2 my-lg-0"
-      style={{ maxHeight: '100px' }}
-      navbarScroll
-    >
-      <Nav.Link onClick={this.logout}>Home</Nav.Link>
-      <Nav.Link href="/uploadcontent">Link</Nav.Link>
-      <NavDropdown title="Link" id="navbarScrollingDropdown">
-        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
-      </NavDropdown>
-      <Nav.Link href="#" disabled>
-        Link
-      </Nav.Link>
-    </Nav>
-    
-  
-       <Navbar.Collapse className="justify-content-end">
-      <Button variant="outline-success">Search</Button>
-      </Navbar.Collapse>
-   
-  </Navbar.Collapse>
-</Navbar>
-{/* ----------- */}
-<Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-  <Navbar.Toggle aria-controls="navbarScroll" />
-  <Navbar.Collapse id="navbarScroll">
-    <Nav
-      className="mr-auto my-2 my-lg-0"
-      style={{ maxHeight: '100px' }}
-      navbarScroll
-    >
-      <Nav.Link href="#action1">Home</Nav.Link>
-      <Nav.Link href="#action2">Link</Nav.Link>
-      <NavDropdown title="Link" id="navbarScrollingDropdown">
-        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
-      </NavDropdown>
-      <Nav.Link href="#" disabled>
-        Link
-      </Nav.Link>
-    </Nav>
-    <Navbar.Collapse className="justify-content-end">
-      <Button variant="outline-success">Search</Button>
-      </Navbar.Collapse>
-  </Navbar.Collapse>
-</Navbar>
-</nav>
+          <Navbar.Collapse id="responsive-navbar-nav">
+            {/* className="justify-content-end" */}
+            <Navbar.Brand href="/dashboard">
+              <h3 style={{ color: "#a018a0", fontWeight: "bolder", marginTop: "2px" }}>E-<span style={{ color: "#51227F" }}>content</span></h3>
+            </Navbar.Brand>
+            <Nav className="mr-auto">
+              <Nav.Item>
+                <Nav.Link href="/dashboard">Home</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="/allchannel">Channel</Nav.Link>
+              </Nav.Item>
+              <NavDropdown style={{ color: "black", fontWeight: "bold" }} title="Categories" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/aboutfounder">Categories 1</NavDropdown.Item>
+                <NavDropdown.Item href="/aboutfounder">Categories 2</NavDropdown.Item>
+                <NavDropdown.Item href="/aboutfounder">Categories 3</NavDropdown.Item>
+                <NavDropdown.Item href="/aboutfounder">Categories 4</NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown style={{ color: "black", fontWeight: "bold" }} title="Company" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/howitwork"><FaServicestack />Services & Pricing</NavDropdown.Item>
+                <NavDropdown.Item href="/howitwork"><FaAngellist />Offers</NavDropdown.Item>
+                <NavDropdown.Item href="/termsandconditions"><FaCcVisa /> Pricing Plans</NavDropdown.Item>
+                <NavDropdown.Item href="/termsandconditions"><FaNewspaper /> Terms & Conditions</NavDropdown.Item>
+
+              </NavDropdown>
+            </Nav>
+            <Form inline>
+              <Button style={{ marginRight: "20px" }} href="/uploadcontent" className="btn btn-primary">Upload</Button>
+            </Form>
+            <Form inline>
+              <Example />
+            </Form>
+          </Navbar.Collapse>
       }
       else {
-        menu =
+        var menu =
           <Navbar.Collapse id="responsive-navbar-nav">
             <Navbar.Brand href="/">
               <h3 style={{ color: "#a018a0", fontWeight: "bolder", marginTop: "2px" }}>E-<span style={{ color: "#51227F" }}>content</span></h3>
@@ -136,12 +111,11 @@ class Header extends Component {
 
 
             </Nav>
-            <Navbar.Collapse className="justify-content-end">
+            <Form>
 
-              <Button style={{ backgroundColor: "#51227F", border: 'none', marginRight: '20px' }} href="/login" className="btn btn-primary" >Sign in<FaSignInAlt /></Button>
+              <Button style={{ backgroundColor: "#51227F" }} href="/login" className="btn btn-primary" >Sign in<FaSignInAlt /></Button>
 
-
-            </Navbar.Collapse>
+            </Form>
           </Navbar.Collapse>
 
 
@@ -150,11 +124,15 @@ class Header extends Component {
 
     }
     return (
-      <div>
-    
+      <Navbar collapseOnSelect expand="lg" bg="white" className="shadow">
+
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
         {menu}
-  
-      </div>
+
+
+
+      </Navbar>
 
     )
   }
