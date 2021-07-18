fd8fe713bddc98251dfe4dfcd0901d5b01c4e15f
Sujit Prasad Kushwaha <57613204+SujitPrasaddKushwaha@users.noreply.github.com>
2021-07-18 16:45:46 +0545

    Update header.js
    confirmation for signout added

--- frontend/econtent/src/header/header.js
+++ frontend/econtent/src/header/header.js
@@ -1,10 +1,10 @@
 import { Component } from "react"
 import { Nav, NavDropdown, Navbar, Form, Button } from 'react-bootstrap';
-import { FaUserGraduate, FaCcVisa, FaAngellist, FaNewspaper, FaServicestack, FaTelegram, FaSignInAlt, FaUsers, FaRegSmile } from 'react-icons/fa'
+import { Modal, ModalDialog, ModalHeader, ModalTitle, ModalBody, ModalFooter} from 'react-bootstrap'
+import { FaTwitter, FaUserGraduate, FaLinkedin, FaInstagram, FaCcVisa, FaAngellist, FaNewspaper, FaServicestack, FaTelegram, FaSignInAlt, FaBell, FaUsers, FaRocketchat, FaSignOutAlt, FaUserCircle, FaPlusCircle, FaRegEye, FaUpload, FaMedapps, FaTelegramPlane, FaUserCog, FaFacebook, FaRegSmile } from 'react-icons/fa'
+import './header.css';
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
-import { Modal, ModalDialog, ModalHeader, ModalTitle, ModalBody, ModalFooter} from 'react-bootstrap'
 
 const id = localStorage.getItem('id')
 
@@ -20,8 +20,8 @@ function Example() {
 
   return (
     <>
-      <Button style={{ marginRight: "20px" }} className="btn btn-danger" onClick={handleShow} >Sign Out</Button>
-
+      {/* <Button style={{ marginRight: "20px" }} className="btn btn-danger" onClick={handleShow} >Sign Out</Button> */}
+      <Button style={{ backgroundColor: "#51227F", color: "white", fontWeight: "bold", marginTop: "5px" }} type="submit" onClick = {handleShow}  className="btn btn-primary"> Sign Out <FaSignOutAlt /> </Button>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Are you sure?</Modal.Title>
@@ -46,46 +46,80 @@ class Header extends Component {
   logout = () => {
     localStorage.removeItem('token')
     localStorage.removeItem('loginstatus')
-    window.locatin.href = '/'
+    window.location.href = '/'
   }
   render() {
     {
       if (localStorage.getItem('token') && localStorage.getItem('loginstatus') === 'true') {
         var menu =
 
-          <Navbar.Collapse id="responsive-navbar-nav">
-            {/* className="justify-content-end" */}
-            <Navbar.Brand href="/dashboard">
-              <h3 style={{ color: "#a018a0", fontWeight: "bolder", marginTop: "2px" }}>E-<span style={{ color: "#51227F" }}>content</span></h3>
-            </Navbar.Brand>
-            <Nav className="mr-auto">
-              <Nav.Item>
-                <Nav.Link href="/dashboard">Home</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link href="/allchannel">Channel</Nav.Link>
-              </Nav.Item>
-              <NavDropdown style={{ color: "black", fontWeight: "bold" }} title="Categories" id="basic-nav-dropdown">
-                <NavDropdown.Item href="/aboutfounder">Categories 1</NavDropdown.Item>
-                <NavDropdown.Item href="/aboutfounder">Categories 2</NavDropdown.Item>
-                <NavDropdown.Item href="/aboutfounder">Categories 3</NavDropdown.Item>
-                <NavDropdown.Item href="/aboutfounder">Categories 4</NavDropdown.Item>
-              </NavDropdown>
-              <NavDropdown style={{ color: "black", fontWeight: "bold" }} title="Company" id="basic-nav-dropdown">
-                <NavDropdown.Item href="/howitwork"><FaServicestack />Services & Pricing</NavDropdown.Item>
-                <NavDropdown.Item href="/howitwork"><FaAngellist />Offers</NavDropdown.Item>
-                <NavDropdown.Item href="/termsandconditions"><FaCcVisa /> Pricing Plans</NavDropdown.Item>
-                <NavDropdown.Item href="/termsandconditions"><FaNewspaper /> Terms & Conditions</NavDropdown.Item>
+        <nav>
+          
+        <nav  class="navbar navbar-expand-md fixed-top-sm justify-content-start striped-border border-bottom">
+        <Nav className="mr-auto">
+        <NavDropdown.Item href="/facebook"><FaFacebook style={{color:"#4169E1"}}/></NavDropdown.Item>
+        <NavDropdown.Item href="/facebook"><FaInstagram style={{color:"#B22222	"}}/></NavDropdown.Item>
+        <NavDropdown.Item href="/facebook"><FaLinkedin style={{color:"#4682B4"}}/></NavDropdown.Item>
+        <NavDropdown.Item href="/facebook"><FaTwitter style={{color:"#00acee"}}/></NavDropdown.Item>
 
-              </NavDropdown>
-            </Nav>
-            <Form inline>
-              <Button style={{ marginRight: "20px" }} href="/uploadcontent" className="btn btn-primary">Upload</Button>
-            </Form>
-            <Form inline>
-              <Example />
-            </Form>
-          </Navbar.Collapse>
+
+ <NavDropdown.Item href="/facebook"><FaBell style={{color:"#FF8C00",width: "20px",height: "20px"}} /></NavDropdown.Item>
+        <NavDropdown.Item href="/facebook"><FaRocketchat style={{color:"green",width: "20px",height: "20px"}} /></NavDropdown.Item>
+        <NavDropdown.Item href="/facebook"><FaUserCircle style={{color:"purple",width: "29px",height: "29px"}}/></NavDropdown.Item>
+        
+
+        {/* <img class="rounded-circle article-img" src="{{ request.user.profile.image.url }}" id="img"/>  */}
+  
+
+</Nav>
+</nav>
+<Navbar.Collapse id="responsive-navbar-nav">
+<Navbar.Brand href="/dashboard">
+           <h3 style={{ color: "#a018a0", fontWeight: "bolder", marginTop: "2px" }}>E-<span style={{ color: "#51227F" }}>content</span></h3>
+         </Navbar.Brand>
+         <Nav className="mr-auto">
+         <div class="collapse navbar-collapse" id="navbarNav">
+ <ul class="navbar-nav">
+   <li class="nav-item active">
+     <a class="nav-link" style={{fontSize:"21px" ,fontWeight: "bolder"}} href="#">Home </a>
+   </li>
+   <li class="nav-item">
+     <a class="nav-link" href="/allchannel" style={{fontSize:"19px" , marginTop: "2px" }} >Channels</a>
+   </li>
+ </ul>
+</div>
+<NavDropdown style={{ color: "black",marginTop: "2px", fontSize:"19px" }} title="Company" id="basic-nav-dropdown">
+             <NavDropdown.Item href="/aboutfounder"><FaUserGraduate /> About Us</NavDropdown.Item>
+             <NavDropdown.Item href="/aboutfounder"><FaRegSmile /> Why Choose Us?</NavDropdown.Item>
+             <NavDropdown.Item href="/Aboutgadgetshop"><FaUsers />Our Team</NavDropdown.Item>
+             <NavDropdown.Item href="/Aboutgadgetshop"><FaTelegram />Carrer With Us</NavDropdown.Item>
+
+           </NavDropdown>
+           <NavDropdown style={{ color: "black", marginTop: "2px", fontSize:"19px"}} title="Services" id="basic-nav-dropdown" >
+             <NavDropdown.Item href="/howitwork"><FaServicestack />Services & Pricing</NavDropdown.Item>
+             <NavDropdown.Item href="/howitwork"><FaAngellist />Offers</NavDropdown.Item>
+             <NavDropdown.Item href="/termsandconditions"><FaCcVisa /> Pricing Plans</NavDropdown.Item>
+             <NavDropdown.Item href="/termsandconditions"><FaNewspaper /> Terms & Conditions</NavDropdown.Item>
+
+           </NavDropdown>
+         </Nav>
+         <div class="collapse navbar-collapse" id="navbarNav"style={{marginRight: "420px" }}  >
+ <ul class="navbar-nav">
+   <li class="nav-item active">
+     <a class="nav-link" style={{fontSize:"18px",marginTop:"2px",fontFamily:"Poppins, sans-serif"}} href="#">Live Audio/Video </a>
+   </li>
+   <li class="nav-item">
+     <a class="nav-link" href="/channel" style={{fontSize:"19px" , marginTop: "2px",fontFamily:"Poppins,sans-serif" }} >UploadContent <FaUpload /></a>
+   </li>
+ </ul>
+</div>
+
+           
+       
+           {/* <Button style={{ marginRight: "20px",backgroundColor:"#51227F" }} href="/login"  onClick = {this.logout} style={{ fontWeight:"bold" }}>Sign Out<FaSignOutAlt /> </Button> */}           
+           <Example />
+        </Navbar.Collapse>
+</nav>
       }
       else {
         var menu =
