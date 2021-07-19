2e3d265775a1b8fd88638970015943c2287cf5cd
Shankar BHandari <iamshankarbhandari@gmail.com>
2021-07-19 20:16:48 +0545

    upadted container

--- frontend/econtent/src/Container/container.js
+++ frontend/econtent/src/Container/container.js
@@ -5,10 +5,6 @@ import Login from './login'
 import Dashboard from '../Dashboard/Dashboard'
 import ChannelsPage from '../ChannelsPage/ChannelsPage';
 import UploadContent from '../UploadContent/uploadcontent';
-import BeforeLogin from './beforeloginpage';
-import AboutUs from './aboutus';
-import WhyChooseUs from './whychooseus';
-import OurTeam from './ourteam';
 
 
 class Container extends Component {
@@ -18,14 +14,10 @@ class Container extends Component {
                 <BrowserRouter>
                     <Switch>
                         <Route exact path="/register" component={Register} />
-                        <Route exact path="/" component={BeforeLogin} />
                         <Route exact path="/login" component={Login} />
                         <Route exact path="/dashboard" component={Dashboard} />
                         <Route exact path="/allchannel" component={ChannelsPage}/>   
-                        <Route exact path="/uploadcontent" component={UploadContent}/>   
-                        <Route exact path="/aboutus" component={AboutUs}/>   
-                        <Route exact path="/whychooseus" component={WhyChooseUs}/>  
-                        <Route exact path="/ourteam" component={OurTeam}/>  
+                        <Route exact path="/uploadcontent" component={UploadContent}/>     
                     </Switch>
                 </BrowserRouter>
 
