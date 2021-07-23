4bbab38f3c60811e2ddf9e957dc30eb3e8b3ab01
Shankar BHandari <iamshankarbhandari@gmail.com>
2021-07-23 20:44:37 +0545

    catagories updated

--- frontend/econtent/src/catagories/catagories.js
+++ frontend/econtent/src/catagories/catagories.js
@@ -1,19 +1,61 @@
 import React from 'react'
 import { Component } from 'react';
-
-class Category extends Component{
-    state={
+import { Card, ListGroup, ListGroupItem, Button } from 'react-bootstrap';
+import axios from 'axios'
+class Category extends Component {
+    state = {
         item: this.props.match.params.item,
+        title: "",
+        video: "",
+        description: "",
+        Categories: "",
+        Price: "",
+        config: {
+            headers: { 'authorization': `Bearer ${localStorage.getItem('token')}` }
+        }
+
+
     }
+    componentDidMount() {
+        axios.get('http://localhost:90/content/catagoris/' + this.state.item).then(
+            (Response)=>{
+
+            }
+        ).catch(
+            error =>{
+                
+            }
+        )
+    }
+
+
     render() {
-        
         return (
             <>
-            <p>ths is catagories page, {this.state.item}</p>
-            
+                <section className="Form my-4 mx-5 pt-5 pb-5">
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+                        <Card.Body>
+                            <Card.Title>{this.state.item}</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                        <ListGroup className="list-group-flush">
+                            <ListGroupItem>Cras justo odio</ListGroupItem>
+                            <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                            <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                        </ListGroup>
+                        <Card.Body>
+                            <Button className="mb-3" style={{ backgroundColor: '#BF3A89', border: 'none' }} variant="primary">BUY Now</Button>
+                        </Card.Body>
+                    </Card>
+                </section>
             </>
         )
     }
+
 }
 
 export default Category
