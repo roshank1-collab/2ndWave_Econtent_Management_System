61f2c161c0fd4de93719b5e32f596b88f97470bd
Shankar BHandari <iamshankarbhandari@gmail.com>
2021-07-23 21:47:50 +0545

    catagories done

--- frontend/econtent/src/catagories/catagories.js
+++ frontend/econtent/src/catagories/catagories.js
@@ -4,12 +4,10 @@ import { Card, ListGroup, ListGroupItem, Button } from 'react-bootstrap';
 import axios from 'axios'
 class Category extends Component {
     state = {
+
+
+        alldata: [],
         item: this.props.match.params.item,
-        title: "",
-        video: "",
-        description: "",
-        Categories: "",
-        Price: "",
         config: {
             headers: { 'authorization': `Bearer ${localStorage.getItem('token')}` }
         }
@@ -17,9 +15,13 @@ class Category extends Component {
 
     }
     componentDidMount() {
-        axios.get('http://localhost:90//content/catagories/' + this.state.item).then(
+        axios.get('http://localhost:90/content/catagories/' + this.state.item).then(
             (response) => {
-                console.log(response.data)
+                this.setState({
+                    alldata: response.data.catagories
+
+                })
+                console.log(this.state.alldata)
             }
         ).catch(
             error => {
@@ -31,28 +33,36 @@ class Category extends Component {
 
     render() {
         return (
-            <>
-                <section className="Form my-4 mx-5 pt-5 pb-5">
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-                        <Card.Body>
-                            <Card.Title>{this.state.item}</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                        <ListGroup className="list-group-flush">
-                            <ListGroupItem>Cras justo odio</ListGroupItem>
-                            <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-                            <ListGroupItem>Vestibulum at eros</ListGroupItem>
-                        </ListGroup>
-                        <Card.Body>
-                            <Button className="mb-3" style={{ backgroundColor: '#BF3A89', border: 'none' }} variant="primary">BUY Now</Button>
-                        </Card.Body>
-                    </Card>
-                </section>
-            </>
+            <div className="container">
+                <div className="row">
+                    {
+                        this.state.alldata.map((item) => {
+                            return (
+                                <Card style={{  width: '310px', margin: '10px'  }}>
+                                    <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+                                    <Card.Body>
+                                        <Card.Title>{item.heading}</Card.Title>
+                                        <Card.Text>
+                                            {item.content_description}
+                                        </Card.Text>
+                                    </Card.Body>
+                                    <Card.Body>
+                                        <Card.Text>
+                                            {item.price}
+                                        </Card.Text>
+                                    </Card.Body>
+                                    <Card.Body>
+                                        <Button className="mb-3" style={{ backgroundColor: '#BF3A89', border: 'none' }} variant="primary">BUY Now</Button>
+                                    </Card.Body>
+                                </Card>
+                            )
+
+                        })
+                    }
+
+                </div>
+
+            </div>
         )
     }
 
