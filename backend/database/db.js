f0c2125c3f090adef04a5568fe494f76ec09b5d0
Roshan Koirala <koiralaroshan678@gmail.com>
2021-08-10 20:01:49 +0545

    no changes

--- backend/database/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose=require('mongoose')
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/E_content_database',{
-    useNewUrlParser:true,
-    useCreateIndex:true,
-    useUnifiedTopology:true
-})
